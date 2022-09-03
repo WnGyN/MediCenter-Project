f324c31f460874d5bdbb477bfcadc7b9dc537717
Gustavo Oliveira <110505589+WnGyN@users.noreply.github.com>
2022-09-03 02:41:24 -0300

    push

---
+++ assets/js/menu.js
@@ -0,0 +1,7 @@
+
+window.onload = function(){
+    document.querySelector(".menuMobile").addEventListener("click", function(){
+        if(document.querySelector(".menu nav ul").style.display == 'flex') {
+
+        }
+    });
