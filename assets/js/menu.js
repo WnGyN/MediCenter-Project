f96eede1a989d460def5adb0bf1fdaad2302f460
Gustavo Oliveira <110505589+WnGyN@users.noreply.github.com>
2022-09-07 18:01:52 -0300

    Arrumando quebra de linha da página
    Arrumado a quebra de linha da página

--- assets/js/menu.js
+++
@@ -1,7 +0,0 @@
-
-window.onload = function(){
-    document.querySelector(".menuMobile").addEventListener("click", function(){
-        if(document.querySelector(".menu nav ul").style.display == 'flex') {
-
-        }
-    });
